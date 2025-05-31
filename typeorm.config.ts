21cb8e34e27faf8995bc0d33a7b9219971d7b86b
nghiemvanmanh <nvmanhfc2003@gmail.com>
2025-05-31 21:50:03 +0700

    hh

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -3,7 +3,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 import 'dotenv/config';
 
-const isProduction: boolean = process.env.NODE_ENV === 'production';
+// const isProduction: boolean = process.env.NODE_ENV === 'production';
 const ConfigDataSource: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
@@ -11,16 +11,18 @@ const ConfigDataSource: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [
-    isProduction
-      ? `${process.cwd()}/database/**/**.entity.js`
-      : `${process.cwd()}/database/**/**.entity.ts`,
-  ],
-  migrations: [
-    isProduction
-      ? `${process.cwd()}/database/migrations/**/*.js`
-      : `${process.cwd()}/database/migrations/**/*.ts`,
-  ],
+  // entities: [
+  //   isProduction
+  //     ? `${process.cwd()}/database/**/**.entity.js`
+  //     : `${process.cwd()}/database/**/**.entity.ts`,
+  // ],
+  // migrations: [
+  //   isProduction
+  //     ? `${process.cwd()}/database/migrations/**/*.js`
+  //     : `${process.cwd()}/database/migrations/**/*.ts`,
+  // ],
+  entities: [`${process.cwd()}/database/**/**.entity.js`],
+  migrations: [`${process.cwd()}/database/migrations/**/*.js`],
   ssl: {
     rejectUnauthorized: false, // Bắt buộc nếu dùng SSL mà không có CA certificate
   },
