b72fa92b2a0fbfaab301dd97943ad712d182f68a
nghiemvanmanh <nvmanhfc2003@gmail.com>
2025-05-31 15:49:41 +0700

    cofig

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -5,11 +5,11 @@ import 'dotenv/config';
 
 const ConfigDataSource: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   migrations: [`${process.cwd()}/database/migrations/**`],
   entities: [`${process.cwd()}/database/**/**.entity.ts`],
 };
